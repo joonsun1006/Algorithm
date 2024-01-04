7597377ccdf4238dc674f4ac6cd082baeca28a24
AnJoonSun <joonsun1006@naver.com>
2024-01-04 19:51:06 +0900

    [Gold V] Title: 수 고르기, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.cc
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -1,22 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a[100002], n, m, e, ret=9987654321;
+int n, m, a[100002], e, ret=1987654321;
 
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	
+	
 	cin >> n >> m;
-
 	for(int i=0; i<n; i++){
 		cin >> a[i];
 	}
 	sort(a, a+n);
-	e = 0;
 	
 	for(int s=0; s<n; s++){
-		while(e < n && a[e]-a[s] < m) e++;
+		while(e<n && a[e]-a[s]<m) e++;
 		if(e == n) break;
 		ret = min(ret, a[e]-a[s]);
 	}
