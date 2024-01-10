ce71193aaa349af3d4adb8f90d9acaaebc75a315
AnJoonSun <joonsun1006@naver.com>
2024-01-10 23:49:19 +0900

    [Bronze V] Title: A-B, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+const num1 = Number(input[0]);
+const num2 = Number(input[1]);
+
+console.log(num1 - num2);
