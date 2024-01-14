631948a43e4a3d640d6ef0bd7311c8c20dee67a1
AnJoonSun <joonsun1006@naver.com>
2024-01-14 23:42:00 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,25 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a[9], sum;
+int a[9];
 
-int main() {
-	//7난쟁이 키의 합 = 100
-	//9C7의 합 == 100
-
-	for (int i = 0; i < 9; i++) {
-		cin >> a[i];//9난쟁이들의 키
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	for(int i=0; i<9; i++){
+		cin >> a[i];
 	}
-
-	sort(a, a + 9);
+	sort(a, a+9);
 	do {
-		sum = 0;
-		for (int i = 0; i < 7; i++) sum += a[i];
-		if (sum == 100) {
-			for (int j = 0; j < 7; j++) cout << a[j] << '\n';
+		int sum = 0;
+		for(int i=0; i<7; i++){
+			sum += a[i];
+		}
+		if(sum == 100){
 			break;
 		}
-	} while (next_permutation(a, a + 9));
-
+	} while(next_permutation(a, a+9));
+	for(int i=0; i<7; i++){
+		cout << a[i] << '\n';
+	}
+	
+	
 	return 0;
 }
