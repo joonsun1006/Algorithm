47f5d9c03895d07ace7502bd560885f16c919bfb
AnJoonSun <joonsun1006@naver.com>
2024-01-19 23:09:06 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 140 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.js
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.js
@@ -1,8 +1,6 @@
 const s = require('fs').readFileSync('/dev/stdin').toString().trim().split('');
-
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
 const counts = new Array(26).fill(0);
 
-s.forEach(el => counts[alphabet.indexOf(el)]++)
+s.forEach(el => counts[el.charCodeAt()-97]++);
 
 console.log(counts.join(" "));
