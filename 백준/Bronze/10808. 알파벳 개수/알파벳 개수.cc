9c632be978553c6ec22b93c5cbf1e9bcddbb167c
AnJoonSun <joonsun1006@naver.com>
2024-02-19 23:33:02 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+int cnt[26];
+
+int main(){
+	cin >> s;
+	for(int i=0; i<s.size(); i++){
+		cnt[s[i]-'a']++;
+	}
+	for(int i : cnt) cout << i << " ";
+	return 0;
+}
