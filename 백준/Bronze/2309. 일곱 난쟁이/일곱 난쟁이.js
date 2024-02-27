2beef02e9ca07874888875c4442b081f73a91aaf
AnJoonSun <joonsun1006@naver.com>
2024-02-27 21:06:55 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.js
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.js
@@ -1,18 +1,32 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number)
 
-//sum - 9C2 === 100
-let ret = [];
+let sum = 0;
 
-const sum = input.reduce((acc, cur) => acc+cur, 0);
+// 전체 합 - 9C2 === 100
+let indexX = 0;
+let indexY = 0;
+let flag = 0;
 
-input.sort((a, b) => a-b);
-for(let i=0; i<8; i++){
-  for(let j=i+1; j<9; j++){
-    if(sum - input[i] - input[j] === 100){
-      ret = input.filter((value) => value !== input[i] && value !== input[j]);
-      // process.exit();
-    }
-  }
+input.sort((a, b) => a-b)
+
+for(let i=0; i<9; i++){
+  sum += input[i];
 }
 
-console.log(ret.join('\n'));
+
+for(let i=0; i<9; i++){
+  for(let j=i+1; j<9; j++){
+    if(sum - input[i] - input[j] === 100){
+      indexX = i; indexY = j;
+      flag=1;
+      break;
+    }
+  }
+  if(flag) break;
+}
+
+for(let i=0; i<9; i++){
+  if(i === indexX || i === indexY) continue;
+  console.log(input[i])
+}
