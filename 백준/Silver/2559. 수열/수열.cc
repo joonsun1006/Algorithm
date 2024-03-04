d6bff61b1930798e560859ec46a71dfed6165942
AnJoonSun <joonsun1006@naver.com>
2024-03-04 21:31:52 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,20 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, k, a[100002], psum[100002], maxval;
+int n, k, temp, psum[100001], sum, ret=-987654321;
 
-int main() {
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
 	cin >> n >> k;
-	maxval = -1000002;
-	for (int i = 1; i <= n; i++) {
-		cin >> a[i];
+	for(int i=1; i<=n; i++){
+		cin >> temp;
+		psum[i] = psum[i-1] + temp;
 	}
-	for (int i = 1; i <= n; i++) {
-		psum[i] = psum[i - 1] + a[i];
+	
+	for(int i=k; i<=n; i++){
+		ret = max(ret, psum[i]-psum[i-k]);
 	}
-	for (int i = 0; i <= n-k; i++) {
-		maxval = max(maxval, psum[i+k] - psum[i]);
-	}
-	cout << maxval << '\n';
+	cout << ret << '\n';
 	return 0;
 }
